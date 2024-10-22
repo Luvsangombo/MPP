715aeafde63d797c6531c3e157027292e484d927
Odkoo Ch <odkoo@Odkoos-MacBook-Air.local>
2024-10-22 00:20:29 -0500

    add Checkout Form

---
+++ project requirements/movie rental/src/ui/CheckOut.java
@@ -0,0 +1,159 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.UIManager;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+import java.awt.Color;
+import javax.swing.JScrollPane;
+import javax.swing.border.LineBorder;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class CheckOut {
+
+    JFrame bframe;
+    DefaultTableModel modelCheckout;
+    DefaultTableModel modelMovie;
+    private JTextField searchtf;
+    private JTable tableCheckout;
+    private JTable tableMovie;
+    private JScrollPane scrollPaneCheckout;
+    private JScrollPane scrollPaneMovie;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    CheckOut window = new CheckOut();
+                    window.bframe.setVisible(true);
+                    window.bframe.setTitle("Check Out Window");
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public CheckOut() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        bframe = new JFrame();
+        bframe.getContentPane().setForeground(new Color(255, 255, 255));
+        bframe.setBounds(100, 100, 800, 700);
+        bframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        bframe.getContentPane().setLayout(null);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(233, 150, 122));
+        panel.setBounds(0, 6, 794, 666);
+        panel.setLayout(null);
+        bframe.getContentPane().add(panel);
+
+        JLabel searchMovieTitleLabel = new JLabel("Search Movie Title");
+        searchMovieTitleLabel.setBounds(6, 365, 150, 16);
+        panel.add(searchMovieTitleLabel);
+
+        searchtf = new JTextField();
+        searchtf.setBounds(129, 360, 161, 26);
+        panel.add(searchtf);
+        searchtf.setColumns(10);
+
+        // Checkout Table
+        scrollPaneCheckout = new JScrollPane();
+        scrollPaneCheckout.setBounds(6, 56, 782, 267);
+        panel.add(scrollPaneCheckout);
+
+        tableCheckout = new JTable();
+        tableCheckout.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int r = tableCheckout.getSelectedRow();
+//                idtf.setText(model.getValueAt(r, 0).toString());
+//                firstnametf.setText(model.getValueAt(r, 1).toString());
+//                lastnametf.setText(model.getValueAt(r, 2).toString());
+
+            }
+        });
+        tableCheckout.setBackground(new Color(255, 240, 245));
+        modelCheckout = new DefaultTableModel();
+        String[] column = {"Movie", "Checkout Date", "Due Date", "Total Amount"};
+        String[] row = new String[4];
+        modelCheckout.setColumnIdentifiers(column);
+        tableCheckout.setModel(modelCheckout);
+        scrollPaneCheckout.setViewportView(tableCheckout);
+
+        // Movie Table
+        scrollPaneMovie = new JScrollPane();
+        scrollPaneMovie.setBounds(6, 390, 782, 267);
+        panel.add(scrollPaneMovie);
+
+        tableMovie = new JTable();
+        tableMovie.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int r = tableMovie.getSelectedRow();
+//                idtf.setText(model.getValueAt(r, 0).toString());
+//                firstnametf.setText(model.getValueAt(r, 1).toString());
+//                lastnametf.setText(model.getValueAt(r, 2).toString());
+
+            }
+        });
+        tableMovie.setBackground(new Color(255, 240, 245));
+        modelMovie = new DefaultTableModel();
+        String[] columnMovie = {"Title", "Format","Genre","Price","Quantity","Availability","Actors","Directors"};
+        String[] rowMovie = new String[8];
+        modelMovie.setColumnIdentifiers(columnMovie);
+        tableMovie.setModel(modelMovie);
+        scrollPaneMovie.setViewportView(tableMovie);
+
+
+        JButton btnadd = new JButton("Checkout");
+        btnadd.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        btnadd.setBounds(6, 15, 117, 29);
+        panel.add(btnadd);
+
+        JButton btndel = new JButton("Delete");
+        btndel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int r = tableCheckout.getSelectedRow();
+                if(r>=0) {
+                    modelCheckout.removeRow(r);
+                    JOptionPane.showMessageDialog(null, "Deleted Successfully");
+
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Please select a row");
+                }
+            }
+        });
+        btndel.setBounds(156, 15, 117, 29);
+        panel.add(btndel);
+    }
+}
